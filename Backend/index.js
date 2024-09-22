adf57731d8e013304b52e145ce59c91bd0b34cdd
yelisetty-suresh-babu <yelisettysuresh2@gmail.com>
2024-09-22 17:26:29 +0530

    first commit

---
+++ Backend/index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/bfhl", (req, res) => {
+  try {
+    const requestBody = req.body; 
+
+    if (!requestBody || !Array.isArray(requestBody.data)) {
+      return res
+        .status(400)
+        .json({ error: "Invalid JSON format. 'data' should be an array." });
+    }
+
+
+    const alpha = requestBody.data.filter((item) => isNaN(item));
+    const nums = requestBody.data.filter((item) => !isNaN(item));
+
+
+    const lowerCaseAlphabets = alpha.filter((item) => /^[a-z]$/.test(item));
+    const highestLowerCaseAlphabet = lowerCaseAlphabets.length
+      ? [lowerCaseAlphabets.sort().reverse()[0]] 
+      : [];
+
+
+    let fileInfo = {
+      file_valid: false,
+    };
+
+    if (requestBody.file_b64) {
+      const base64String = requestBody.file_b64;
+      const buffer = Buffer.from(base64String, "base64");
+
+      const fileType = "application/octet-stream"; 
+      const fileSize = buffer.length;
+
+      fileInfo = {
+        file_valid: true,
+        file_type: fileType,
+        file_size: fileSize,
+      };
+    }
+
+
+    return res.json({
+      is_success: true,
+      user_id: "abd_17122002",
+      college_email_id: "abd@srmap.edu.in",
+      college_roll_no: "AP21110011283",
+      numbers: nums,
+      alphabets: alpha,
+      highest_lowercase_alphabet: highestLowerCaseAlphabet,
+      ...fileInfo,
+    });
+  } catch (error) {
+    console.error("Error processing the request:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/bfhl", (req, res) => {
+  return res.json({ operation_code: 1 });
+});
+
+app.listen(3000, () => {
+  console.log(`Server running on port ${3000}`);
+});
---
+++ mahesh/src/App.jsx
@@ -0,0 +1,126 @@
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+import Select from "react-select";
+
+const App = () => {
+  const [input, setInput] = useState("");
+  const [numbers, setNumbers] = useState([]);
+  const [alphas, setAlphas] = useState([]);
+  const [highestAlphabet, setHighestAlphabet] = useState("");
+  const [error, setError] = useState("");
+  const [filter, setFilter] = useState({
+    numbers: false,
+    alpha: false,
+    highestAlphabet: false,
+  });
+
+  const options = [
+    { value: "alpha", label: "Alphabets" },
+    { value: "numbers", label: "Numbers" },
+    { value: "highestAlphabet", label: "Highest Alphabet" },
+  ];
+  const customStyles = {
+    option: (provided, state) => ({
+      ...provided,
+      fontSize: 14,
+      color: "black",
+      backgroundColor: "white",
+    }),
+  };
+  const handleSelectChange = (selectedOptions) => {
+    const updatedFilter = {
+      numbers: false,
+      alpha: false,
+      highestAlphabet: false,
+    };
+    console.log(selectedOptions);
+
+    selectedOptions.forEach((option) => {
+      updatedFilter[option.value] = true;
+    });
+
+    setFilter(updatedFilter);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const parsedInput = JSON.parse(input);
+
+      if (!Array.isArray(parsedInput.data)) {
+        throw new Error(
+          "Invalid JSON format. Expecting 'data' to be an array."
+        );
+      }
+
+      setError("");
+      console.log("Filter and Input:", filter, parsedInput);
+
+      const res = await axios.post("http://localhost:3000/bfhl", parsedInput);
+
+      console.log(res.data);
+
+      setNumbers(res.data.numbers || []);
+      setAlphas(res.data.alphabets);
+      setHighestAlphabet(res.data.highest_lowercase_alphabet || "");
+    } catch (e) {
+      console.error(e);
+      setError("Invalid JSON input. Please check your input format.");
+    }
+  };
+
+  const toggleFilter = (filterName) => {
+    setFilter((prevFilter) => ({
+      ...prevFilter,
+      [filterName]: !prevFilter[filterName],
+    }));
+  };
+
+  return (
+    <div className="container">
+      <div className="input-section">
+        <label htmlFor="api-input">API Input (JSON)</label>
+        <textarea
+          id="api-input"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          rows="5"
+          className="p-3 w-full"
+          placeholder=""
+        />
+        <button
+          onClick={handleSubmit}
+          className="bg-blue-500 my-1 py-1 text-white"
+        >
+          Submit
+        </button>
+        {error && <div className="error-message text-red-500">{error}</div>}
+      </div>
+
+      <p>Multi-Filter</p>
+      <Select
+        isMulti
+        name="filters"
+        options={options}
+        styles={customStyles}
+        onChange={handleSelectChange}
+        className="basic-multi-select"
+        classNamePrefix="select"
+      />
+
+      <div className="response-section">
+        <div className="py-2">
+          <strong>Filtered Response</strong>
+        </div>
+        {filter.numbers && <div>Numbers: {numbers.join(", ")}</div>}
+        {filter.alpha && <div>Alphabets: {alphas.join(", ")}</div>}
+        {filter.highestAlphabet && (
+          <div>Highest lowercase alphabet: {highestAlphabet}</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
